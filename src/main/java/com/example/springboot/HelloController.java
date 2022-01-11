25feba16c0b815f6251c43f7f755618ef519ef1e
sreeramsunkara <sr_sunkara@yahoo.com>
2022-01-11 14:07:27 -0500

    version 3

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings Developers from Spring Boot + Tanzu!  Version 2";
+		return "Greetings Developers from Spring Boot + Tanzu!  Version 3";
 	}
 
 }
