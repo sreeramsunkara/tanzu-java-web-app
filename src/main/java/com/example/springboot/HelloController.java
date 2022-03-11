553e66f645bb646d1f3dbb4b71f3e2e7d054fccd
sreeramsunkara <sr_sunkara@yahoo.com>
2022-03-11 17:16:54 -0500

    removed version

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings Developers from Spring Boot + Tanzu!  Version 6";
+		return "Greetings Developers from Spring Boot + Tanzu!";
 	}
 
 }
