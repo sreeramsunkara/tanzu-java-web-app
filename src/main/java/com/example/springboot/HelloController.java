dc1cd3b4d3dcba31213ece0ff4c591b02495f160
sreeramsunkara <sr_sunkara@yahoo.com>
2022-07-12 10:44:16 -0400

    message update

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings Developers from Spring Boot + TAP";
+		return "Greetings from Spring Boot + Tanzu!";
 	}
 
 }
